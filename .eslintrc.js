a872818002f1eabe129ba750065fab4227a71c3f
Bipin C <63953352+Bunty9@users.noreply.github.com>
2024-03-24 02:03:12 +0530

    folder structure, icons, data, assets

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // existing rules...
+    'linebreak-style': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 };
